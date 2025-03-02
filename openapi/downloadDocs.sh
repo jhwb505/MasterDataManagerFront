fe3bab3634c844bee7eafebf9364b539f6335f25
fnaoya <dzi20815@gmail.com>
2025-03-02 22:02:05 +0900

    APIクライアントコード自動生成用ファイル作成

--- openapi/downloadDocs.sh
+++ openapi/downloadDocs.sh
@@ -2,7 +2,7 @@
 
 API_DOCS_URL="http://localhost:8080/docs/api-docs.yaml"
 OUTPUT_DIR="./openapi"
-OUTPUT_FILE="${OUTPUT_DIR}/openapi-docs.yaml"
+OUTPUT_FILE="${OUTPUT_DIR}/api-docs.yaml"
 
 curl -o "$OUTPUT_FILE" "$API_DOCS_URL"
 
